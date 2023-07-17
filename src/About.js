f7c148cfff8054c4e95e7950c53bac715917f450
christopherflores9312 <christopherflores9312@gmail.com>
2023-07-17 14:03:05 -0700

    about styled

--- src/About.js
+++ src/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './About.module.css';
 // import avatar from './path_to_your_image'; // replace with the path to your image
 
 function About() {
   return (
-    <section>
+    <section className={styles.about}>
       {/* <img src={avatar} alt="Your Name" /> */}
       <h2>About Me</h2>
       <p>Your bio goes here...</p>
